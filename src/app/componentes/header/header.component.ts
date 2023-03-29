243f002686887f664277ae578ed39259a72b5552
vjp-EduardoNC <eneilac02@informatica.iesvalledeljerteplasencia.es>
2023-03-29 20:25:18 +0200

    RoleGuard Terminado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -27,8 +27,8 @@ export class HeaderComponent implements OnInit, DoCheck {
   public grupoTeleoperador: string;
   isAdmin: boolean;
 
-  subdominioNombre= environment.subdominio.nombre;
-  subdominioColor= environment.subdominio.color;
+  subdominioNombre = environment.subdominio.nombre;
+  subdominioColor = environment.subdominio.color;
 
   constructor(private auth: AuthService, private profileService: ProfileService,
               private cargarAlarma: CargaAlarmaService, private router: Router) {
@@ -46,133 +46,132 @@ export class HeaderComponent implements OnInit, DoCheck {
 
       })
 
-      this.isAdmin = !this.auth.admin;
+      this.isAdmin = this.auth.isAdmin();
+    }
+  }
+
+  //Compruebo si esta login para ocultar el navbar
+  ngDoCheck():
+    void {
+    this.estaLogin = this.auth.isLoggedIn()
+
+
+  }
+
+  //Toast para el Alert indicando que la operación fue exitosa
+  alertExito()
+    :
+    void {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      //El tiempo que permanece la alerta, se obtiene mediante una variable global en environment.ts
+      timer: environment.timerToast,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
       }
-    }
+    })
 
-    //Compruebo si esta login para ocultar el navbar
-    ngDoCheck():
+    Toast.fire({
+      icon: 'success',
+      title: environment.fraseAlarmaAceptada,
+    })
+  }
+
+  //Toast para el alert indicando que hubo algún error en la operación
+  alertError()
+    :
     void {
-      this.estaLogin = this.auth.isLoggedIn()
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: environment.timerToast,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
 
+    Toast.fire({
+      icon: 'error',
+      title: environment.fraseErrorAsignarAlarma
+    })
+  }
 
-    }
-
-    //Toast para el Alert indicando que la operación fue exitosa
-    alertExito()
-  :
-    void {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        //El tiempo que permanece la alerta, se obtiene mediante una variable global en environment.ts
-        timer: environment.timerToast,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-
-      Toast.fire({
-        icon: 'success',
-        title: environment.fraseAlarmaAceptada,
-      })
-    }
-
-    //Toast para el alert indicando que hubo algún error en la operación
-    alertError()
-  :
-    void {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: environment.timerToast,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-
-      Toast.fire({
-        icon: 'error',
-        title: environment.fraseErrorAsignarAlarma
-      })
-    }
-
-    //creamomos el metodo que lanzará el modal
-    modalAlarma(msg
-  :
-    any
+  //creamomos el metodo que lanzará el modal
+  modalAlarma(msg
+                :
+                any
   ):
     void {
-      //obtenemos el usuario logeado
-      this.profileService.getProfile()
-        .subscribe((resp: IProfileUser[]) => {
-            //obtenemos el nombre del grupo al que pertenece
-            this.grupoTeleoperador = resp[0].groups[0].name
-            //lanzamos el modal solo si pertenece a los teleoperadores
-            if (this.grupoTeleoperador == "teleoperador") {
-              //asignamos el valor de action a una variable
-              this.accion = msg['action']
-              //asignamos el valor de alarma a otra variable
-              this.alarmaAModificar = msg['alarma']
-              //iniciamos el modal mostrando la id y comprobando la procedencia de la alarma
-              Swal.fire({
-                html: '<h3>¡Atención! Nueva alarma desde ' + this.comprobarProcedencia(this.alarmaAModificar) + '<h3>' +
-                  '<p class="left">Identificador de alarma: ' + this.alarmaAModificar.id + '</p>' +
-                  '<p class="left">' + this.comprobarProcedenciaTitular(this.alarmaAModificar) + '</p>' +
-                  '<p class="left"> Tipo de alarma: ' + this.alarmaAModificar.id_tipo_alarma.nombre + '</p>' +
-                  '<p class="left">¿Desea Asignarse esta alarma?</p>',
-                showCancelButton: true,
-                confirmButtonText: 'Aceptar',
-              }).then((result) => {
-                /* Read more about isConfirmed, isDenied below */
-                if (result.isConfirmed) {
-                  this.asignarTeleoperador(this.alarmaAModificar)
-                }
+    //obtenemos el usuario logeado
+    this.profileService.getProfile()
+      .subscribe((resp: IProfileUser[]) => {
+          //obtenemos el nombre del grupo al que pertenece
+          this.grupoTeleoperador = resp[0].groups[0].name
+          //lanzamos el modal solo si pertenece a los teleoperadores
+          if (this.grupoTeleoperador == "teleoperador") {
+            //asignamos el valor de action a una variable
+            this.accion = msg['action']
+            //asignamos el valor de alarma a otra variable
+            this.alarmaAModificar = msg['alarma']
+            //iniciamos el modal mostrando la id y comprobando la procedencia de la alarma
+            Swal.fire({
+              html: '<h3>¡Atención! Nueva alarma desde ' + this.comprobarProcedencia(this.alarmaAModificar) + '<h3>' +
+                '<p class="left">Identificador de alarma: ' + this.alarmaAModificar.id + '</p>' +
+                '<p class="left">' + this.comprobarProcedenciaTitular(this.alarmaAModificar) + '</p>' +
+                '<p class="left"> Tipo de alarma: ' + this.alarmaAModificar.id_tipo_alarma.nombre + '</p>' +
+                '<p class="left">¿Desea Asignarse esta alarma?</p>',
+              showCancelButton: true,
+              confirmButtonText: 'Aceptar',
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                this.asignarTeleoperador(this.alarmaAModificar)
+              }
 
-              })
-            }
+            })
           }
-        )
+        }
+      )
 
-    }
+  }
 
-    // con este metodo se asigna el teleoperador a la alarma y con el servicio se
-    // modifica la alarma
-    asignarTeleoperador(alarma)
-  :
+  // con este metodo se asigna el teleoperador a la alarma y con el servicio se
+  // modifica la alarma
+  asignarTeleoperador(alarma)
+    :
     void {
-      this.profileService.getProfile()
-        .subscribe((resp: IProfileUser[]) => {
-            this.teleoperador = resp[0].id
-            this.alarmaAModificar.id_teleoperador = this.teleoperador
-            this.cargarAlarma.modificarAlarma(this.alarmaAModificar).subscribe(
-              e => {
-                this.router.navigate(['alarmas/aceptada/modificar', alarma.id])
-              },
-              error => {
-                this.alertError()
-              })
-          }
-        )
+    this.profileService.getProfile()
+      .subscribe((resp: IProfileUser[]) => {
+          this.teleoperador = resp[0].id
+          this.alarmaAModificar.id_teleoperador = this.teleoperador
+          this.cargarAlarma.modificarAlarma(this.alarmaAModificar).subscribe(
+            e => {
+              this.router.navigate(['alarmas/aceptada/modificar', alarma.id])
+            },
+            error => {
+              this.alertError()
+            })
+        }
+      )
 
 
-    }
+  }
 
-    //comprobamos el tipo de action que nos llega por parametro
-    comprobarAccion(msg)
-  :
+  //comprobamos el tipo de action que nos llega por parametro
+  comprobarAccion(msg)
+    :
     void {
-      //si es una alarma nueva abre el modal de alarma
+    //si es una alarma nueva abre el modal de alarma
 
-      if(msg['action'] == 'new_alarm'
-  )
-    {
+    if (msg['action'] == 'new_alarm'
+    ) {
       this.modalAlarma(msg)
     }
     // si una alarma fue asignada ya se cierra el modal
@@ -182,35 +181,33 @@ export class HeaderComponent implements OnInit, DoCheck {
     }
   }
 
-    //comprobamos si está a null pacientes ucr para devolver un string
-    comprobarProcedenciaTitular(msg)
-  :
-    string
-    {
-      if (msg.id_paciente_ucr) {
-        //si no es null devolvemos el paciente ucr con su nombre
-        return 'Titular: ' + msg.id_paciente_ucr.id_persona.nombre + ' ' + msg.id_paciente_ucr.id_persona.apellidos
-      }
-
-      //si no ese null el terminal devolvemos  su numero y el titular del mismo
-      if (msg.id_terminal)
-        return 'Titular: ' + msg.id_terminal.id_titular.id_persona.nombre + ' ' + msg.id_terminal.id_titular.id_persona.apellidos + '' +
-          '\nTerminal ' + msg.id_terminal.numero_terminal
+  //comprobamos si está a null pacientes ucr para devolver un string
+  comprobarProcedenciaTitular(msg)
+    :
+    string {
+    if (msg.id_paciente_ucr) {
+      //si no es null devolvemos el paciente ucr con su nombre
+      return 'Titular: ' + msg.id_paciente_ucr.id_persona.nombre + ' ' + msg.id_paciente_ucr.id_persona.apellidos
     }
 
-    comprobarProcedencia(msg)
-  :
-    string
-    {
-      if (msg.id_paciente_ucr) {
-        //si no es null devolvemos paciente
-        return 'UCR'
-      }
-
-      //si no ese null devolvemos terminal
-      if (msg.id_terminal)
-        return 'Terminal'
-    }
-
-
+    //si no ese null el terminal devolvemos  su numero y el titular del mismo
+    if (msg.id_terminal)
+      return 'Titular: ' + msg.id_terminal.id_titular.id_persona.nombre + ' ' + msg.id_terminal.id_titular.id_persona.apellidos + '' +
+        '\nTerminal ' + msg.id_terminal.numero_terminal
   }
+
+  comprobarProcedencia(msg)
+    :
+    string {
+    if (msg.id_paciente_ucr) {
+      //si no es null devolvemos paciente
+      return 'UCR'
+    }
+
+    //si no ese null devolvemos terminal
+    if (msg.id_terminal)
+      return 'Terminal'
+  }
+
+
+}
