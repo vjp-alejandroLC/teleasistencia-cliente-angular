e5999795a1d9407e5d14b5aa3beed8e71fa9eb83
vjp-EduardoNC <eneilac02@informatica.iesvalledeljerteplasencia.es>
2023-04-17 23:58:22 +0200

    Acabando datos personales en formulario reactivo

--- src/app/componentes/persona/crear-persona/crear-persona.component.ts
+++ src/app/componentes/persona/crear-persona/crear-persona.component.ts
@@ -9,7 +9,7 @@ import Swal from "sweetalert2";
 import {Direccion} from "../../../clases/direccion";
 import {CargaDireccionService} from "../../../servicios/carga-direccion.service";
 import {environment} from "../../../../environments/environment";
-import {FormBuilder, FormGroup, MaxValidator, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -19,31 +19,92 @@ import {FormBuilder, FormGroup, MaxValidator, Validators} from "@angular/forms";
 })
 
 export class CrearPersonaComponent implements OnInit {
+
+  /**
+   * Atributos
+   */
   public persona: IPersona;
   //public direcciones: IDireccion[];
-  public fecha_actual = new Date();
-  public anno_actual = this.fecha_actual.getFullYear();
-  public mes_actual = this.fecha_actual.getMonth() + 1;
-  public dia_actual = this.fecha_actual.getDate();
   public dire: IDireccion;
   public formulario: FormGroup;
 
+
+  /**
+   * Constantes
+   */
+  readonly REGEX_NAME = /^[A-Z][a-zA-ZÀ-ÿ- ]+$/;
+  readonly REGEX_DNI = /^([0-9]{8})([A-Z])$/;
+  readonly REGEX_MOVIL = /^[6|7]{1}[ ]*([0-9][ ]*){8}$/;
+  readonly REGEX_FIJO = /^[9]{1}[ ]*([0-9][ ]*){8}$/;
+  readonly REGEX_CP = /[0-9]+$/;
+  readonly REGEX_TEXT = /^[^\s]+(\s.*)?$/;
+
+  /**
+   * Constructor
+   * @param titleService
+   * @param route
+   * @param cargaPersonas
+   * @param router
+   * @param cargaDirecciones
+   * @param formBuilder
+   */
   constructor(private titleService: Title, private route: ActivatedRoute, private cargaPersonas: CargaPersonaService, private router: Router, private cargaDirecciones: CargaDireccionService, private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
-    //Formularios reactivos
-    this.buildForm();
-
-
+    this.buildForm();  //Formularios reactivos
     this.titleService.setTitle('Crear nueva persona');
     this.persona = new Persona();
-    //this.direcciones = this.route.snapshot.data['direcciones'];
     this.persona.sexo = 'Hombre';
     this.persona.telefono_fijo = '';
     this.persona.telefono_movil = '';
     this.dire = new Direccion();
   }
+
+  /**
+   *  Getters del Formulario reactivo para los banners de error.
+   */
+  get dni() {
+    return this.formulario.get('dni');
+  }
+
+  get nombre() {
+    return this.formulario.get('nombre');
+  }
+
+  get apellidos() {
+    return this.formulario.get('apellidos');
+  }
+
+  get date() {
+    return this.formulario.get('date');
+  }
+
+  get telefono_fijo() {
+    return this.formulario.get('telefono_fijo')
+  }
+
+  get telefono_movil() {
+    return this.formulario.get('telefono_movil')
+  }
+
+  get localidad() {
+    return this.formulario.get('localidad')
+  }
+
+  get provincia() {
+    return this.formulario.get('provincia')
+  }
+
+  get direccion() {
+    return this.formulario.get('direccion')
+  }
+
+  get text() {
+    return this.formulario.get('text_area')
+  }
+
+
 
   nuevaDireccion(): void {
     this.cargaDirecciones.nuevaDireccion(this.dire).subscribe(
@@ -56,20 +117,23 @@ export class CrearPersonaComponent implements OnInit {
     );
   }
 
-
   nuevaPersona(): void {
     this.persona.id_direccion = this.dire;
     this.cargaPersonas.nuevaPersona(this.persona).subscribe(
       e => {
         this.nuevaDireccion();
         this.alertExito()
-        this.router.navigate(['/personas']);
       },
       error => {
         this.alertError()
       }
     );
   }
+
+
+  /**
+   * Alertas
+   */
 
   //Toast para el Alert indicando que la operación fue exitosa
   alertExito(): void {
@@ -113,48 +177,48 @@ export class CrearPersonaComponent implements OnInit {
   }
 
 
-  //formularios reactivos
+  /**
+   *   formularios reactivos
+   */
   private buildForm() {
     this.formulario = this.formBuilder.group({
-      nombre:['',[Validators.required,
-                Validators.maxLength(200),
-                Validators.pattern("^[\w'\-,.][^0-9_!¡?÷?¿(\)\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$")],
-             ],
-      apellidos:[''],
-
-      dni:['',[Validators.required,
-              Validators.maxLength(9),
-              Validators.pattern("((([X-Z])|([LM])){1}([-]?)((\d){7})([-]?)([A-Z]{1}))|((\d{8})([-]?)([A-Z]))")],
-              ],
-
-      fecha_nacimiento:['',[Validators.required]],
-
-      sexo:['',[Validators.required]],
-
-      telefono_fijo:['',[Validators.maxLength(12),
-                        Validators.pattern("^[6|7]{1}[ ]*([0-9][ ]*){8}$")]],
-
-      localidad:['',[Validators.required,
-                    Validators.maxLength(200)]],
-
-      telefono_movil:['',[Validators.maxLength(12),
-                        Validators.pattern("^[6|7]{1}[ ]*([0-9][ ]*){8}$")]],
-
-      provincia:['',[Validators.required,
-                    Validators.maxLength(200)]],
-
-      direccion:['',[Validators.required,
-                    Validators.maxLength(200),]],
-
-      codigo_postal:['',[Validators.maxLength(5),
-                        Validators.minLength(5),
-                        Validators.pattern("[0-9]+$")]]
+      nombre: ['', [Validators.required,
+        Validators.maxLength(200),
+        Validators.minLength(2),
+        Validators.pattern(this.REGEX_NAME)],
+      ],
+      apellidos: ['', [Validators.required,
+        Validators.maxLength(200),
+        Validators.minLength(2),
+        Validators.pattern(this.REGEX_NAME)],
+      ],
+      dni: ['', [Validators.required,
+        Validators.maxLength(9),
+        Validators.pattern(this.REGEX_DNI)],
+      ],
+      date: ['', [Validators.required]],
+      sexo: ['Hombre', [Validators.required]],
+      telefono_fijo: ['', [Validators.maxLength(12),
+        Validators.pattern(this.REGEX_FIJO)]],
+      localidad: ['', [Validators.required,
+        Validators.maxLength(200)]],
+      telefono_movil: ['', [Validators.maxLength(12),
+        Validators.pattern(this.REGEX_MOVIL)]],
+      provincia: ['', [Validators.required,
+        Validators.maxLength(200)]],
+      direccion: ['', [Validators.required,
+        Validators.maxLength(200),]],
+      codigo_postal: ['', [Validators.maxLength(5),
+        Validators.minLength(5),
+        Validators.pattern(this.REGEX_CP)]],
+      tipo_user: ['Persona mayor', [Validators.required]],
+      text_area: ['', [Validators.pattern(this.REGEX_TEXT)]]
     });
   }
 
-  comprobarFormulario(){
-      console.log(this.formulario.value)
-  }
 
+  comprobarFormulario() {
+    console.log(this.formulario.value)
+  }
 
 }
