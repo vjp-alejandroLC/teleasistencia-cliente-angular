aaacd3f0ea244ec05abab20e003bc97160c7923a
vjp-ferminph <72455008+vjp-ferminph@users.noreply.github.com>
2023-05-16 19:14:12 +0200

    punto 11: añadido popup de cookies

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -29,6 +29,7 @@ export class HeaderComponent implements OnInit, DoCheck {
   public grupoTeleoperador: string;
   public clasificacionRecursos: IClasificacioRecurso[] | []; // Esta variable la utilizaremos para obtener la clasificacion de recursos que mostraremos en el apartado de Recursos
 
+  public cookiesAceptadas: boolean;
 
   isAdmin: boolean;
 
@@ -58,15 +59,14 @@ export class HeaderComponent implements OnInit, DoCheck {
         () => console.log('Fin de observable')
       )
     }
+    this.cookiesAceptadas=true; //esto desactiva el popput de cookies
   }
-
   //Compruebo si esta login para ocultar el navbar
 
   ngDoCheck():
     void {
     this.isLoggedIn = this.auth.isLoggedIn()
     this.isAdmin = this.auth.isAdmin();
-
   }
 
   //Toast para el Alert indicando que la operación fue exitosa
@@ -220,5 +220,8 @@ export class HeaderComponent implements OnInit, DoCheck {
       return 'Terminal'
   }
 
+  aceptarCookies(): void{
+    this.cookiesAceptadas=true;
+  }
 
 }
